c3992c257ff991928aa70592688a5e5fc817c750
LusineNazaryan <nazaryanlusine@yahoo.com>
2017-10-24 01:02:40 -0700

    Add files via upload

--- parser.cpp
+++ parser.cpp
@@ -1,113 +1,153 @@
 #include "parser.h"
 
-CParser::CParser(QDataStream& out, QVector<SDataToken> dataTokens, QVector<SCodeToken> codeTokens) :
-												m_out(out), m_dataTokens(dataTokens), m_codeTokens(codeTokens) 
+CParser::CParser(QVector<SDataToken> dataTokens, QVector<SCodeToken> codeTokens) :
+m_dataTokens(dataTokens), m_codeTokens(codeTokens)
 {
 }
-
-void CParser::work(QString fileName)
+void CParser::work(CReaderWriter* writer, QString filename)
 {
-	writeHeader(m_out);
+//	CReaderWriter* writer = new CReaderWriter;
 
-	if (!m_dataTokens.isEmpty())
-	{
-		writeDataTokens(m_out);
-	}
-	writeCodeTokens(m_out);
+//	writer->addSection(writeHeader(writer->getSignature(), writer -> getVersion()), filename);
+	writer->addSection(writeDataSection());
+	writer->addSection(writeCodeSection());
+	writer->addSection(writeDataTableSection());
+	writer->addSection(writeCodeTableSection());
+	writer->write(filename);
 }
+//
+//QByteArray CParser::writeHeader(QString signature, int32 version)
+//{
+//	SHeader header = createHeader(signature, version);
+//	QByteArray out;
+//
+//	out.append(header.signature);
+//	out.append(header.version);
+//	out.append(header.recordCount);
+//
+//	for (int i = 0; i < header.recordCount; ++i)
+//	{
+//		out.append(static_cast<int>(header.recordTable[i].type));
+//		out.append(header.recordTable[i].offset);
+//		out.append(header.recordTable[i].size);
+//	}
+//	return out;
+//}
+//SHeader CParser::createHeader(QString signature, int32 version)
+//{
+//	QString signature = signature;
+//	int version = version;
+//	int offset = sizeof signature + sizeof(int);
+//
+//	SHeader header(signature, version);
+//
+//	if (!m_dataTokens.isEmpty())
+//	{
+//		SHeaderTable headerTable(ESectionType::Data, offset, m_dataTokens.size());
+//		header.recordTable.push_back(headerTable);
+//		offset += writeDataSection().size();
+//	}
+//
+//	{
+//		SHeaderTable headerTable(ESectionType::Code, offset, m_codeTokens.size());
+//		header.recordTable.push_back(headerTable);
+//		offset += writeCodeSection().size();
+//	}
+//
+//	if (!m_dataTokens.isEmpty())
+//	{
+//		SHeaderTable headerTable(ESectionType::DataTable, offset, m_dataTokens.size());
+//		header.recordTable.push_back(headerTable);
+//		offset += writeDataTableSection().size();
+//	}
+//
+//	/*
+//	{
+//	SHeaderTable headerTable(ESectionType::CodeTable, offset, m_dataTokens.size());
+//	header.arrayTable.push_back(headerTable);
+//	offset += writeCodeTableSection(m_out);
+//	}
+//
+//	*/
+//	header.recordCount = header.recordTable.size();
+//	header.size = offset;
+//	return header;
+//}
 
-SHeader CParser::createHeader()
+QByteArray CParser::writeCodeSection()
 {
-	QString signature = "SAED/VirtualMachine/ExeFile";
-	int version = 1;
-	int offset = sizeof signature + sizeof(int);
-
-	SHeader header(signature, version);
-
-	if (!m_dataTokens.isEmpty())
-	{
-		SHeaderTable headerTable(ESctionType::Data, offset, m_dataTokens.size());
-		header.arrayTable.push_back(headerTable);
-		offset += m_dataTokens.size();
-	}
-
-	{
-		SHeaderTable headerTable(ESctionType::Code, offset, m_codeTokens.size());
-		header.arrayTable.push_back(headerTable);
-		offset += m_codeTokens.size();
-	}
-
-	if (!m_dataTokens.isEmpty())
-	{
-		SHeaderTable headerTable(ESctionType::SymbolTable, offset, m_dataTokens.size());
-		header.arrayTable.push_back(headerTable);
-		offset += m_dataTokens.size();
-	}
-	/*Then When writng the debugger, will write the debugInfo section too*/
-	/*
-	{
-	SHeaderTable headerTable(ESctionType::DebugInfo, offset, m_dataTokens.size());
-	header.arrayTable.push_back(headerTable);
-	offset += m_dataTokens.size();
-	}
-	*/
-
-	header.recordCount = header.arrayTable.size();
-
-}
-void CParser::writeHeader(QDataStream& out)
-{
-	SHeader header = createHeader();
-
-	out << header.signature;
-	out << header.version;
-	out << header.recordCount;
-
-	for (int i = 0; i < header.recordCount; ++i)
-	{
-		out << header.arrayTable[i].type;
-		out << header.arrayTable[i].offset;
-		out << header.arrayTable[i].size;
-	}
-}
-
-void CParser::writeCodeTokens(QDataStream& out)
-{
+	QByteArray out;
+	int offset = 0;			//size of codeSection
 	for (int i = 0; i < m_codeTokens.size(); i++)
 	{
-		out << m_codeTokens[i].opcode;
-
-		QString instructionName = m_codeTokens[i].name;
-		int count = inst[instructionName].argument_count;
-		for (int j = 0; j < count; ++j)
+		out.append(m_codeTokens[i].opcode.opcode);
+		offset += sizeof m_codeTokens[i].opcode.opcode;
+		for (int j = 0; j < m_codeTokens[i].argValue.size(); j++)
 		{
-			out << m_codeTokens[i].arg_value[j];
+			out.append(m_codeTokens[i].argValue[j]);
+			offset += sizeof m_codeTokens[i].argValue[j];
 		}
-		
 	}
+	return out;
 }
 
-void CParser::writeDataTokens(QDataStream& out)
+QByteArray CParser::writeDataSection()
 {
-	int name = 5;
-	for (QVector<SDataToken>::iterator it = m_dataTokens.begin(); it != m_dataTokens.end(); ++it)
+	QByteArray out;
+	int offset = 0;		//size of dataSection
+	uint8 zero = 0;
+	int remainder = 0;
+	for (int i = 0; i < m_dataTokens.size(); i++)
 	{
-		//write it.size
-		out << static_cast<int>((*it).size);
-		//write name
-		out << name;
-		m_nameGrammar[(*it).name] = name;
-		if (!(*it).value.isNull())
+		m_nameGrammar[m_dataTokens[i].identifierName] = offset;
+		int dataType = static_cast<int>(m_dataTokens[i].type);
+		if (dataType)
 		{
-			//write it.value
-			out << (*it).value;
-			name += 4;
+			int typeToSize = static_cast<int> (pow(2, dataType));
+			remainder = offset % typeToSize;
+			offset += typeToSize - remainder;
 		}
-		name += 4;
+		else
+
+		{
+			remainder = 0;
+		}
+		while (remainder != 0)
+		{
+			out.append(zero);
+			remainder--;
+		}
+		out.append(m_dataTokens[i].value);
+		offset += pow(2, dataType);
 	}
+	return out;
 }
+QByteArray CParser::writeDataTableSection()
+{
+	QByteArray out;
+	int offset = 0;
+	for (int i = 0; i < m_dataTokens.size(); i++)
+	{
+		int dataType = static_cast<int>(m_dataTokens[i].type);
+		out.append(dataType);
+		out.append(m_dataTokens[i].identifierName);
+		out.append(m_dataTokens[i].value);
+		out.append(m_dataTokens[i].line);
+		offset += sizeof(int) + m_dataTokens[i].identifierName.size() +
+			/*dataType +*/ sizeof(int);
+	}
+	return out;
+}
+QByteArray CParser::writeCodeTableSection()
+{
+	QByteArray out;
+	int offset = 0;
+	/*for (int i = 0; i < m_codeTokens.size(); i++)
+	{
+	out << m_codeTokens[i].opcode.opcode;
 
-
-
-
+	}*/
+//	return offset;
+	return out;
+}
 
--- parser.h
+++ parser.h
@@ -1,62 +1,34 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include "readerwriter.h"
 #include "lexer.h"
+//#include "buffer.h"
 #include <QTextStream>
 #include <QVector>
 #include <qdebug.h>
 #include "instructions.h"
 #include <QDataStream>
 #include <QFileInfo>
+#include <cmath>
 
 using uint16 = unsigned short;
-
-enum ESctionType {
-	Data, Code, SymbolTable, DebugInfo
-};
-
-struct SHeaderTable {
-	SHeaderTable(ESctionType tt, int oo, int ss) : type(tt), offset(oo), size(ss)
-	{
-	}
-	SHeaderTable() {}
-	ESctionType type;
-	int offset;
-	int size;
-};
-
-struct SHeader {
-	SHeader(QString ss, int vv) :signature(ss), version(vv)
-	{
-	}
-	QString signature;
-	int version;
-	int recordCount;
-	QVector<SHeaderTable> arrayTable;
-};
 
 class CParser
 {
 public:
-	CParser(QDataStream& out, QVector<SDataToken> dataTokens, QVector<SCodeToken> codeTokens);
-	void work(QString fileName);
-
+	CParser::CParser(QVector<SDataToken> dataTokens, QVector<SCodeToken> codeTokens);
+	void work(CReaderWriter* writer, QString filename);
+public:
+	QByteArray writeDataSection();
+	QByteArray writeCodeSection();
+	QByteArray writeDataTableSection();
+	QByteArray writeCodeTableSection();
 private:
-	void writeHeader(QDataStream&);
-	SHeader createHeader();
-	void writeDataTokens(QDataStream&);
-	void writeCodeTokens(QDataStream&);
-	uint16 getArgCode(QString argName);
-private:
-	//	SHeader m_SHeader;
 	QVector<SDataToken> m_dataTokens;
 	QVector<SCodeToken> m_codeTokens;
 	QHash<QString, int> m_nameGrammar;
-	QDataStream m_out;
-
 };
-
-
 
 
 #endif
