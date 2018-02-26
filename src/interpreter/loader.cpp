7db21136fbd9ffd129269352d61598a0d1a0d0ac
NorikYenokyan <norikyenokyan@gmail.com>
2018-02-26 23:05:06 +0400

    changed loader coding style

--- src/interpreter/loader.cpp
+++ src/interpreter/loader.cpp
@@ -1,24 +1,28 @@
 #include "loader.h"
 
-uint32 CLoader::load(const QString& filePath, CMemory& memory) {
-    CRW reader;
-    reader.read(filePath);
-    QByteArray code = reader.getSection(CRW::ESectionType::Code);
-    QByteArray data = reader.getSection(CRW::ESectionType::Data);
+uint32 CLoader::load(const QString& filePath, CMemory& memory)
+{
+	CRW reader;
+	reader.read(filePath);
+	QByteArray code = reader.getSection(CRW::ESectionType::Code);
+	QByteArray data = reader.getSection(CRW::ESectionType::Data);
 
-    if (code.size() + data.size() >= memory.getSize()) {
-        throw "error. memory has not enough size";
-    }
+	if (code.size() + data.size() >= memory.getSize())
+	{
+		throw "error. memory has not enough size";
+	}
 
-    uint32 index = 0;
+	uint32 index = 0;
 
-    for (uint32 i = 0; i < data.size(); ++i) {
-        memory[index++] = data[i];
-    }
+	for (uint32 i = 0; i < data.size(); ++i)
+	{
+		memory[index++] = data[i];
+	}
 
-    for (uint32 i = 0; i < code.size(); ++i) {
-        memory[index++] = code[i];
-    }
+	for (uint32 i = 0; i < code.size(); ++i)
+	{
+		memory[index++] = code[i];
+	}
 
-    return data.size();
+	return data.size();
 }
